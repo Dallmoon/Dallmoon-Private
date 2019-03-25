289aa1adf3a89ba989192f4011804d950424d2a7
Dallmoon <yunhunjun007@gmail.com>
2019-03-26 00:13:52 +0900

    fixed detail

--- Teamproject_whale/Dallmoon_script.js
+++ Teamproject_whale/Dallmoon_script.js
@@ -1,14 +1,69 @@
 $(document).ready(function(){
 
     // console.log($(".D_sideicon").mouseenter)
-    $(".D_sideicon1").mouseenter(function(){
-        $('.D_sideicon_sub').css("display","inline-block");
+    $(".D_quick_search").mouseenter(function(){
+        $('.D_quick_search_sub').css("display","inline-block");
 
     });
-    $(".D_sideicon1").mouseout(function(){
-        $('.D_sideicon_sub').css("display","none");
+    $(".D_quick_search").mouseout(function(){
+        $('.D_quick_search_sub').css("display","none");
     });
 
+
+
+
+    $(".D_memo").mouseenter(function(){
+        $('.D_memo_sub').css("display","inline-block");
+
+    });
+    $(".D_memo").mouseout(function(){
+        $('.D_memo_sub').css("display","none");
+    });
+
+
+    $(".D_translation").mouseenter(function(){
+        $('.D_translation_sub').css("display","inline-block");
+
+    });
+    $(".D_translation").mouseout(function(){
+        $('.D_translation_sub').css("display","none");
+    });
+
+
+    $(".D_plus").mouseenter(function(){
+        $('.D_plus_sub').css("display","inline-block");
+
+    });
+    $(".D_plus").mouseout(function(){
+        $('.D_plus_sub').css("display","none");
+    });
+
+
+    $(".D_tools").mouseenter(function(){
+        $('.D_tools_sub').css("display","inline-block");
+    });
+    $(".D_tools").mouseout(function(){
+        $('.D_tools_sub').css("display","none");
+    });
+
+
+    $(".D_music").mouseenter(function(){
+        $('.D_music_sub').css("display","inline-block");
+
+    });
+    $(".D_music").mouseout(function(){
+        $('.D_music_sub').css("display","none");
+    });
+
+
+
+    $(".D_bookmark").mouseenter(function(){
+        $('.D_bookmark_sub').css("display","inline-block");
+
+    });
+    $(".D_bookmark").mouseout(function(){
+        $('.D_bookmark_sub').css("display","none");
+    });
 
 
     $(".D_sideicon").click(function(){
