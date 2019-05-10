8f31cf246e92e4fd7196f9efa40a1b3b06ffd78e
Dallmoon <yunhunjun007@gmail.com>
2019-05-10 14:02:36 +0900

    fixed absol exe-function

--- 이웅모 강사님/leeteacher/제어문 연습문제.js
+++ 이웅모 강사님/leeteacher/제어문 연습문제.js
@@ -1,173 +1,202 @@
 // 문제 1
-// const x = 15;
-// if (x < 10 && x > 20) {
-//   console.log(x);
-// }
-
+// (function foo() {
+//   const x = 15;
+//   if (x < 10 && x > 20) {
+//     console.log(x);
+//   }
+// }());
 // 문제 2
-// for (let i = 0; i < 10; i++) {
-//   if (i % 2 === 0) {
-//     console.log(i);
+// (function foo() {
+//   for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
 //   }
-// }
-
+// }());
 // 문제 3
-// let str = '';
-// for (let i = 0; i < 10; i++) {
-//   if (i % 2 === 0) {
-//     str += i;
+// (function foo() {
+//   let str = '';
+//   for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//       str += i;
+//     }
 //   }
-// }
-// console.log(str);
-
+//   console.log(str);
+// }());
 // 문제 4
-// for (let i = 0; i < 10; i++) {
-//   if (i % 2 !== 0) {
-//     console.log(10 - i);
+// (function foo() {
+//   for (let i = 0; i < 10; i++) {
+//     if (i % 2 !== 0) {
+//       console.log(10 - i);
+//     }
 //   }
-// }
+// }());
 
 // 문제 5
-// let x = 0;
-// while (x < 10) {
-//   if (x % 2 === 0) {
-//     console.log(x);
-//     x += 2;
+// (function foo() {
+//   let x = 0;
+//   while (x < 10) {
+//     if (x % 2 === 0) {
+//       console.log(x);
+//       x += 2;
+//     }
 //   }
-// }
-
+// }());
 // 문제 6
-// let x = 0;
-// while (x < 10) {
-//   if (x % 2 !== 0) {
-//     console.log(10 - x);
+// (function foo() {
+//   let x = 0;
+//   while (x < 10) {
+//     if (x % 2 !== 0) {
+//       console.log(10 - x);
+//     }
+//     x += 1;
 //   }
-//   x += 1;
-// }
+// }());
 
 // 문제 7
-// let sum = 0;
-// for (let i = 0; i < 10; i++) {
-//   sum += i;
-// }
-// console.log(sum);
 
+// (function foo() {
+//   let sum = 0;
+//   for (let i = 0; i < 10; i++) {
+//     sum += i;
+//   }
+//   console.log(sum);
+// }());
 // 문제 8
 
-// let sum = 0;
-// for (let i = 0; i < 20; i++) {
-//   if (i % 2 !== 0 && i % 3 !== 0) {
-//     sum += i;
+// (function foo() {
+//   let sum = 0;
+//   for (let i = 0; i < 20; i++) {
+//     if (i % 2 !== 0 && i % 3 !== 0) {
+//       sum += i;
+//     }
 //   }
-// }
-// console.log(sum);
-
+//   console.log(sum);
+// }());
 
 // 문제 9
-// let sum = 0;
-// for (let i = 0; i < 20; i++) {
-//   if (i % 2 === 0 || i % 3 === 0) {
-//     sum += i;
+// (function foo() {
+//   let sum = 0;
+//   for (let i = 0; i < 20; i++) {
+//     if (i % 2 === 0 || i % 3 === 0) {
+//       sum += i;
+//     }
 //   }
-// }
-// console.log(sum);
+//   console.log(sum);
+// }());
 
 // 문제 10
 
-// let li = [];
-// for (let i = 1; i < 6; i++) {
-//   for (let j = 1; j < 6; j++) {
-//     if (i + j === 6) {
-//       li = [i, j];
-//       console.log(li);
+// (function foo() {
+//   let li = [];
+//   for (let i = 1; i < 6; i++) {
+//     for (let j = 1; j < 6; j++) {
+//       if (i + j === 6) {
+//         li = [i, j];
+//         console.log(li);
+//       }
 //     }
 //   }
-// }
+// }());
 
 // 삼각형 pattern 1
-// let str = '';
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 1; j <= 5; j++) {
-//     if (i >= j) {
-//       str += '*';
+
+// (function foo() {
+//   let str = '';
+//   for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= 5; j++) {
+//       if (i >= j) {
+//         str += '*';
+//       }
 //     }
+//     str += '\n';
 //   }
-//   str += '\n';
-// }
-// console.log(str);
+//   console.log(str);
+// }());
 
 // 삼각형 패턴 2
 
-// let str = '';
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 1; j <= 5; j++) {
-//     if (j < i) {
-//       str += ' ';
-//     } else {
-//       str += '*';
+// (function foo() {
+//   let str = '';
+//   for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= 5; j++) {
+//       if (j < i) {
+//         str += ' ';
+//       } else {
+//         str += '*';
+//       }
 //     }
+//     str += '\n';
 //   }
-//   str += '\n';
-// }
-// console.log(str);
+//   console.log(str);
+// }());
 
 // 삼각형 pattern 3
-// let str = '';
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 5; j >= 1; j--) {
-//     if (i <= j) {
-//       str += '*';
+
+// (function foo() {
+//   let str = '';
+//   for (let i = 1; i <= 5; i++) {
+//     for (let j = 5; j >= 1; j--) {
+//       if (i <= j) {
+//         str += '*';
+//       }
 //     }
+//     str += '\n';
 //   }
-//   str += '\n';
-// }
-// console.log(str);
+//   console.log(str);
+// }());
 
 // 삼각형 pattern 4
 
-// let str = '';
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 1; j <= 5; j++) {
-//     if (j < 6 - i) {
-//       str += ' ';
-//     } else {
-//       str += '*';
+// (function foo() {
+//   let str = '';
+//   for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= 5; j++) {
+//       if (j < 6 - i) {
+//         str += ' ';
+//       } else {
+//         str += '*';
+//       }
 //     }
+//     str += '\n';
 //   }
-//   str += '\n';
-// }
-// console.log(str);
+//   console.log(str);
+// }());
 
 // 정삼각형
 
-// let str = '';
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 1; j <= 5; j++) {
-//     if (j < 6 - i) {
-//       str += ' ';
-//     } else if (j === 5) {
-//       str += '*';
-//     } else {
-//       str += '**';
+// (function foo() {
+//   let str = '';
+//   for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= 5; j++) {
+//       if (j < 6 - i) {
+//         str += ' ';
+//       } else if (j === 5) {
+//         str += '*';
+//       } else {
+//         str += '**';
+//       }
 //     }
+//     str += '\n';
 //   }
-//   str += '\n';
-// }
-// console.log(str);
+//   console.log(str);
+// }());
 
 // 역정삼각형
 
-// let str = '';
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 1; j <= 5; j++) {
-//     if (j < i) {
-//       str += ' ';
-//     } else if (j === 5) {
-//       str += '*';
-//     } else {
-//       str += '**';
+// (function foo() {
+//   let str = '';
+//   for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= 5; j++) {
+//       if (j < i) {
+//         str += ' ';
+//       } else if (j === 5) {
+//         str += '*';
+//       } else {
+//         str += '**';
+//       }
 //     }
+//     str += '\n';
 //   }
-//   str += '\n';
-// }
-// console.log(str);
+//   console.log(str);
+// }());
