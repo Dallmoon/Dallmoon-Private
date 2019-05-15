e5283e1491b1f949e0629ee3c272be872aad9b2d
Dallmoon <yunhunjun007@gmail.com>
2019-05-15 18:03:03 +0900

    commit

--- 이웅모 강사님/leeteacher/foo_2.js
+++ 이웅모 강사님/leeteacher/foo_2.js
@@ -20,3 +20,24 @@
 // obj.a = 1;
 // obj.b = 10;
 // 의미 없음
+
+var value = 1;
+// const 키워드로 선언한 변수 value는 전역 객체의 프로퍼티가 아니다.
+// const value = 1;
+
+const obj = {
+  value: 100,
+  foo() {
+    console.log("foo's this: ",  this);  // obj {value: 100, foo: ƒ}
+    console.log("foo's this.value: ",  this.value); // 100
+
+    // 메소드의 중첩 함수 내부의 this에는 전역 객체가 바인딩된다.
+    function bar() {
+      console.log("bar's this: ",  this); // window
+      console.log("bar's this.value: ", this.value); // 1
+    }
+    bar();
+  }
+};
+
+obj.foo();
