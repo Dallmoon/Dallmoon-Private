e8fb3077f31f11a61e004e6d9745c5d1cb4a97c9
Dallmoon <yunhunjun007@gmail.com>
2019-07-01 19:31:38 +0900

    popupUI & algorithm

--- 이웅모 강사님/leeteacher/Project/server.js
+++ 이웅모 강사님/leeteacher/Project/server.js
@@ -5,8 +5,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 4500;
 
 // Static File Service
--- 이웅모 강사님/leeteacher/알고리즘 연습문제.js
+++ 이웅모 강사님/leeteacher/알고리즘 연습문제.js
@@ -219,8 +219,8 @@
 // console.log(testArray); // [ -1, 1, 3, -2, 2, 0 ]
 // console.log(specialSort(testArray)); // [ -1, -2, 0, 1, 2, 3 ]
 
-function getDayName(a, b){
+// function getDayName(a, b){
 
-}
+// }
 
-console.log(getDayName(5, 24)); // TUE
+// console.log(getDayName(5, 24)); // TUE
